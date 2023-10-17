754f2f33c4f99c7f73a470a8221195dc4bae9d8e
Igor Sobral <igorgmz1421@gmail.com>
2023-10-17 20:11:56 -0300

    add: api services

--- src/App.js
+++ src/App.js
@@ -2,11 +2,26 @@ import { useState } from "react";
 import { FiSearch } from "react-icons/fi";
 import "./styles.css";
 
-function App() {
-  const [input, setInput] = useState("");
+import api from "./services/api";
 
-  function handleSearch() {
-    alert(input);
+function App() {
+  
+  const [input, setInput] = useState("");
+  const [cep, setCep] = useState('');
+
+  async function handleSearch() {
+    if (input === "") {
+      
+      alert("Digite um CEP!");
+      return;
+    }
+console.log("🚀 ~ file: App.js:13 ~ handleSearch ~ input:", input)
+    try {
+      const response = await api.get(`${input}/json`)
+      setCep(response.data)
+    } catch {
+      alert("ERRO");
+    }
   }
 
   return (
@@ -18,8 +33,7 @@ function App() {
           type="text"
           placeholder="Digite um CEP..."
           value={input}
-          onChange={(e) => 
-            setInput(e.target.value)}
+          onChange={(e) => setInput(e.target.value)}
         />
 
         <button className="buttonSearch" onClick={handleSearch}>
@@ -28,12 +42,12 @@ function App() {
       </div>
 
       <main className="main">
-        <h2>CEP:</h2>
+        <h2>CEP: {cep.cep}</h2>
 
-        <span>Rua exemplo</span>
-        <span>Bairro exemplo</span>
-        <span>vila exemplo</span>
-        <span>local Exemplo</span>
+        <span>Rua: {cep.logradouro}</span>
+        <span>Complemento: {cep.complemento}</span>
+        <span>Bairro: {cep.bairro}</span>
+        <span>Localidade: {`${cep.localidade} - ${cep.uf}`}</span>
       </main>
     </div>
   );
