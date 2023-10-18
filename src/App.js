c89d84117820b2e985ad8eb55d6ac98e262127ab
Igor Sobral <igorgmz1421@gmail.com>
2023-10-17 23:08:40 -0300

    add:  main panel gradient color

--- src/App.js
+++ src/App.js
@@ -5,22 +5,19 @@ import "./styles.css";
 import api from "./services/api";
 
 function App() {
-  
   const [input, setInput] = useState("");
-  const [cep, setCep] = useState('');
+  const [cep, setCep] = useState({});
 
   async function handleSearch() {
     if (input === "") {
-      
       alert("Digite um CEP!");
       return;
     }
-console.log("🚀 ~ file: App.js:13 ~ handleSearch ~ input:", input)
     try {
-      const response = await api.get(`${input}/json`)
-      setCep(response.data)
+      const response = await api.get(`${input}/json`);
+      setCep(response.data);
     } catch {
-      alert("ERRO");
+      alert("error ");
     }
   }
 
@@ -41,14 +38,16 @@ console.log("🚀 ~ file: App.js:13 ~ handleSearch ~ input:", input)
         </button>
       </div>
 
-      <main className="main">
-        <h2>CEP: {cep.cep}</h2>
+      {Object.keys(cep).length > 0 && (
+        <main className="main">
+          <h2>CEP: {cep.cep}</h2>
 
-        <span>Rua: {cep.logradouro}</span>
-        <span>Complemento: {cep.complemento}</span>
-        <span>Bairro: {cep.bairro}</span>
-        <span>Localidade: {`${cep.localidade} - ${cep.uf}`}</span>
-      </main>
+          <span>{cep.logradouro}</span>
+          <span>{cep.complemento}</span>
+          <span>{cep.bairro}</span>
+          <span>Localidade: {`${cep.localidade} - ${cep.uf}`}</span>
+        </main>
+      )}
     </div>
   );
 }
